9674be470c721073ac415918ad2fc17f9a6cea2f
jeffyactive <jeff@reelyactive.com>
2016-10-06 19:55:44 -0400

    Improved story fetching

--- js/bubblescape.js
+++ js/bubblescape.js
@@ -35,24 +35,30 @@ angular.module('bubblescape', ['reelyactive.cormorant',
   // Create the devices based on the demo type
   switch(demo) {
     case 'everything':
-      $scope.devices['00'] = JEFF_FITBIT_COWORKING;
+      $scope.devices['000000000000'] = JEFF_FITBIT_COWORKING;
       break;
     case 'gym':
-      $scope.devices['00'] = JEFF_FITBIT_GYM;
-      $scope.devices['01'] = BRIAN_IPHONE_GYM;
-      $scope.devices['02'] = BENOIT_REELYAPP_GYM;
+      $scope.devices['000000000000'] = JEFF_FITBIT_GYM;
+      $scope.devices['000000000010'] = BRIAN_IPHONE_GYM;
+      $scope.devices['000000000020'] = BENOIT_REELYAPP_GYM;
       break;
   }
 
   // Fetch all the stories
-  for(device in $scope.devices) {
-    var deviceUrl = $scope.devices[device].event.deviceUrl;
-    var receiverUrl = $scope.devices[device].event.receiverUrl;
-
-    cormorant.getCombinedStory(deviceUrl, receiverUrl, function(story) {
-      $scope.devices[device].story = story;
-    });
+  function getStories() {
+    for(device in $scope.devices) { 
+      if(!$scope.devices[device].hasOwnProperty('story')) {
+        var deviceUrl = $scope.devices[device].event.deviceUrl;
+        var receiverUrl = $scope.devices[device].event.receiverUrl;
+        cormorant.getCombinedStory(deviceUrl, receiverUrl, function(story) {
+          $scope.devices[device].story = story;
+          getStories();  // Get the next story
+        });
+      }
+    }
   }
+
+  getStories();
 
   // Verify if the device's story has been fetched
   $scope.hasFetchedStory = function(device) {
